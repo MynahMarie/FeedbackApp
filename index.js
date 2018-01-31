5c3c16f228efcc98f3bc2d51b3e53600fcecb518
MynahMarie <mynah.marie@gmail.com>
2018-01-31 13:42:14 +0200

    Set up billing functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
 const passport = require('passport');
 const keys = require('./config/keys');
 const mongoose = require('mongoose');
@@ -9,6 +10,8 @@ require('./services/passport');
 mongoose.connect(keys.mongodbURI);
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
@@ -21,9 +24,18 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Listening on port 5000`);
 });
